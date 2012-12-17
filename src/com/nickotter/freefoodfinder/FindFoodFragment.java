236f1a9ccd9f6be7f859ac8b1112df584c1f653d
Nick Otter <otternq@gmail.com>
2012-12-17 01:45:53 -0800

    added a map to the FindFoodFragment

--- src/com/nickotter/freefoodfinder/FindFoodFragment.java
+++ src/com/nickotter/freefoodfinder/FindFoodFragment.java
@@ -1,5 +1,6 @@
 package com.nickotter.freefoodfinder;
 
+import com.google.android.gms.maps.MapFragment;
 import com.nickotter.freefoodfinder.R;
 
 import android.app.Fragment;
@@ -10,14 +11,19 @@ import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class FindFoodFragment extends Fragment {
+public class FindFoodFragment extends MapFragment {
 
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    /*public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-    	this.setHasOptionsMenu(true);
+    	
     	
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.activity_main, container, false);
+    }*/
+    
+    public void onCreate(Bundle savedInstanceState) {
+    	super.onCreate(savedInstanceState);
+    	this.setHasOptionsMenu(true);
     }
     
     @Override
